13793c3fe2756d9e4ae45f650f894b921bc1ccfa
faaiz <faaizajaz@gmail.com>
2020-08-29 16:17:45 +0500

    updated search view logic

--- bookquery/views.py
+++ bookquery/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from .forms import SearchForm
+#from 
 
 # Create your views here.
 def SearchView(request, **kwargs):
 	if request.method == 'POST':
 		form = SearchForm(request.POST)
 		if form.is_valid():
-			print("something")
+			query = form.save()
+			results = query.run_query_any()
+			
+			# Now redirect to a different page (or use ajax) to display all 
+			# the returned results, and allow user to choose.
 
 	else:
 		form = SearchForm()
