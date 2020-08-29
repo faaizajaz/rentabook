e36b8f3a1b177242160af893caf61b9d013d47c1
faaiz <faaizajaz@gmail.com>
2020-08-29 16:03:20 +0500

    Made search view

--- bookquery/views.py
+++ bookquery/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import SearchForm
 
 # Create your views here.
+def SearchView(request, **kwargs):
+	if request.method == 'POST':
+		form = SearchForm(request.POST)
+		if form.is_valid():
+			print("something")
+
+	else:
+		form = SearchForm()
+
+	return render(request, 'bookquery/search.html', {'form': form})
