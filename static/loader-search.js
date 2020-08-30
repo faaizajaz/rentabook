4d046c338b368abf942b3e4123d908774548e181
faaiz <faaizajaz@gmail.com>
2020-08-30 14:13:38 +0500

    Added loading animation for search

---
+++ static/loader-search.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	//when search button is clicked
+	$(".search-button").click(function(e){
+		//Show loading animation
+		$(".loader-div").html("<div class='loader'></div>")
+	});
+});
