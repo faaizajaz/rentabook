2d93ab47182202e8f95df86cd633cf672d32acc2
faaiz <faaizajaz@gmail.com>
2020-08-30 14:13:14 +0500

    Added comment

--- bookquery/models.py
+++ bookquery/models.py
@@ -5,9 +5,13 @@ from libgenapi.libgen_search import LibgenSearch
 # this is set in the search view
 
 class BookQuery(models.Model):
+	# Search fields
 	title = models.CharField(max_length=1000, verbose_name='Title of book')
 	author = models.CharField(max_length=1000, verbose_name='Author name')
 	any_field = models.CharField(max_length=1000, verbose_name='Wildcard search')
+
+	# Fields populated after search
+
 
 	def __str__(self):
 		return(f"Query for {self.title} by {self.author}")
