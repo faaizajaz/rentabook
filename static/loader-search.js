6f331577aa688ca4849cfc5c9485676f018b5c9b
faaiz <faaizajaz@gmail.com>
2020-08-30 17:55:18 +0500

    Tidied search js

--- static/loader-search.js
+++ static/loader-search.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	//when search button is clicked
-	$(".search-button").click(function(e){
+	$(".search-button").click(function(){
 		//Show loading animation
 		$(".loader-div").html("<div class='loader'></div>")
 	});
