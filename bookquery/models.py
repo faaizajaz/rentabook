a966ca787c1c892ad76974f1f027c9cee026ce8f
faaiz <faaizajaz@gmail.com>
2020-08-31 15:27:38 +0500

    added user FK to query

--- bookquery/models.py
+++ bookquery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from libgenapi.libgen_search import LibgenSearch
+from django.contrib.auth.models import User
 
 # Give the query model a results field that holds the dict of results (also includes urls)
 # this is set in the search view
@@ -7,15 +8,17 @@ from libgenapi.libgen_search import LibgenSearch
 class BookQuery(models.Model):
 	search_prompt = "Enter as many keywords about the book as possible"
 	# Search fields
-	title = models.CharField(max_length=1000, verbose_name='Title of book')
-	author = models.CharField(max_length=1000, verbose_name='Author name')
 	any_field = models.CharField(max_length=1000, verbose_name=search_prompt)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 	# Fields populated after search
 
 
 	def __str__(self):
-		return(f"Query for {self.title} by {self.author}")
+		try:
+			return(f"'{self.any_field}' by {self.user.username}")
+		except AttributeError:
+			return(f"'{self.any_field}' by unknown user")
 
 	def run_query_title(self):
 		search = LibgenSearch()
