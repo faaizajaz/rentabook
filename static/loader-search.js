71e931feca09d9c2ebe820df5496feafceb3fb03
faaiz <faaizajaz@gmail.com>
2020-08-31 18:17:36 +0500

    Added text loading message

--- static/loader-search.js
+++ static/loader-search.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
 	//when search button is clicked
 	$(".search-button").click(function(){
 		//Show loading animation
-		$(".loader-div").html("<div class='loader'></div>")
+		$(".loader-div").html("<span class='loading-text'>Please be patient, this could take a minute</span><div class='loader'></div>")
 	});
 });
