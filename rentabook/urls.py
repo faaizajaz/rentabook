a65a36a6fbdd1648c110e27306537cd7a7d748a7
faaiz <faaizajaz@gmail.com>
2020-09-01 17:25:57 +0500

    obfuscated login and register urls

--- rentabook/urls.py
+++ rentabook/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', bq_views.SearchView, name="search"),
-    path('register/', user_views.RegisterUser, name="register"),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('iwantin/', user_views.RegisterUser, name="register"),
+    path('gimme/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('downloadAPI/<book_id>', bq_views.DownloadView.as_view(), name='download-book-api'),
 ]
