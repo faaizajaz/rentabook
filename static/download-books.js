194dee017e7bd5271dd20527da77e72fbfa409e4
faaiz <faaizajaz@gmail.com>
2020-09-01 13:44:17 +0500

    Prettified

--- static/download-books.js
+++ static/download-books.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-	$(".download-button").click(function(event) {
-		event.preventDefault();
-		$(".loader-div").html("<span class='loading-text'>Download in progress. Please be patient, this could take up to 5 minutes</span><div class='loader'></div>");
+    $(".download-button").click(function(event) {
+        event.preventDefault();
+        $(".loader-div").html("<span class='loading-text'>Download in progress. Please be patient, this could take up to 5 minutes</span><div class='loader'></div>");
 
-		$.each($("input[name='search-result']:checked"), function(){
-			//alert($(this).attr("book-id"));
+        $.each($("input[name='search-result']:checked"), function() {
+            //alert($(this).attr("book-id"));
 
-			//Do ajax to download, convert, and email
-			var downloadURL = $(this).attr("download-api");
+            //Do ajax to download, convert, and email
+            var downloadURL = $(this).attr("download-api");
 
-			$.ajax({
-				type: 'post',
-				url: downloadURL,
-				data: {
-					'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
-				},
-				dataType: 'json',
-				success: function(data) {
-					//alert(data['Title']);
-					$(".loader-div").html("<h1>Success! " + data['Title'] + " was downloaded");
+            $.ajax({
+                type: 'post',
+                url: downloadURL,
+                data: {
+                    'csrfmiddlewaretoken': $("input[name=csrfmiddlewaretoken]").val()
+                },
+                dataType: 'json',
+                success: function(data) {
+                    //alert(data['Title']);
+                    $(".loader-div").html("<h1>Success! " + data['Title'] + " was downloaded");
 
-				}
-			})
-		});
-	});
+                }
+            })
+        });
+    });
 });
